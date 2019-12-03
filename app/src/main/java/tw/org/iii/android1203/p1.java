d92f410991760ea027a3f39ba1ed9f1d06e07209
BearWinder <wer45685265@gmail.com>
2019-12-03 11:53:19 +0800

    3

--- app/src/main/java/tw/org/iii/android1203/p1.java
+++ app/src/main/java/tw/org/iii/android1203/p1.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ViewFlipper;
 
 
 /**
@@ -15,17 +16,37 @@ import android.view.ViewGroup;
  */
 public class p1 extends Fragment {
 
-
-    public p1() {
-        // Required empty public constructor
-    }
-
-
+private  View mainView;
+private ViewFlipper viewFlipper;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        if(mainView == null){
+            mainView = inflater.inflate(R.layout.fragment_p1,container,false);
+            viewFlipper = mainView.findViewById(R.id.viewflipper);
+
+            View f0 = viewFlipper.getChildAt(0);
+            View f1 = viewFlipper.getChildAt(1);
+            View f2 = viewFlipper.getChildAt(2);
+            View f3 = viewFlipper.getChildAt(3);
+            MyClickListener listener = new MyClickListener();
+            f0.setOnClickListener(listener);
+            f1.setOnClickListener(listener);
+            f2.setOnClickListener(listener);
+            f3.setOnClickListener(listener);
+
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_p1, container, false);
+        return mainView;
+    }
+    private  class MyClickListener implements  View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            viewFlipper.showNext();
+        }
     }
 
 }
